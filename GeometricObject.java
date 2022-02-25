c5d76a9f79244ece8726c8532bf9f9b5756f17c7
Darren <93760891+Dwdr@users.noreply.github.com>
2022-02-25 18:02:22 +0800

    Add files via upload

---
+++ GeometricObject.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+public class GeometricObject {
+    private String color;
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObject(){
+        color = "white";
+        filled = false;
+        dateCreated = new Date();
+    }
+
+    public GeometricObject(String color, boolean filled){
+        setColor(color);
+        setFilled(filled);
+        setDateCreated(new Date());
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String toString() {
+        return getDateCreated().toString();
+    }
+}
