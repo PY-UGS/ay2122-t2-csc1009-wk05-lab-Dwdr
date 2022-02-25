09849766684f33b3381647934b68d7afa35132f9
Darren <93760891+Dwdr@users.noreply.github.com>
2022-02-25 18:34:33 +0800

    Delete Triangle.java

--- Triangle.java
+++
@@ -1,9 +0,0 @@
-public class Triangle extends Shape{
-    public Triangle(double height, double base){
-        super(height, base);
-    }
-    public double area(){
-        System.out.println("Inside Area for Triangle.");
-        return 0.5 * this.getDim1() * this.getDim2();
-    }
-}
