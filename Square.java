2d3b136445f5f4911ce4e609ca27346f3520d89c
Darren <93760891+Dwdr@users.noreply.github.com>
2022-02-25 18:34:20 +0800

    Delete Square.java

--- Square.java
+++
@@ -1,9 +0,0 @@
-public class Square extends Shape{
-    public Square(double length, double width){
-        super(length, width);
-    }
-    public double area(){
-        System.out.println("Inside Area for Square.");
-        return this.getDim1() * this.getDim2();
-    }
-}
