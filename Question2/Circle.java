9e2e50ea9bc10be6373f22debbd82bcb438d0d73
Darren <93760891+Dwdr@users.noreply.github.com>
2022-02-25 18:32:53 +0800

    Create Circle.java

---
+++ Question2/Circle.java
@@ -0,0 +1,11 @@
+public class Circle extends Shape{
+    public Circle(double radius1, double radius2){
+        super(radius1, radius2);
+    }
+
+    public double area(){
+        System.out.println("Inside Area for Circle.");
+        return this.getPI() * this.getDim1() * this.getDim2();
+    }
+}
+
